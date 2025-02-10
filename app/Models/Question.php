7b624115d2833610d39479bdb2bf46bf4f5f3c7f
Julio Retkwa <julio@zoocha.com>
2025-02-09 21:34:20 -0300

    HEY-17: add factory dependency to pass tests

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,8 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Question extends Model
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+    use Notifiable;
 }
